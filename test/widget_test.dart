62e08431a44ccd4ed51616540b1abfb04d53457d
otakuanime756-svg <otaku.anime756@gmail.com>
2026-08-14 15:09:43 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:quran_app/app.dart';
 
 void main() {
-  testWidgets('Quran app starts', (tester) async {
+  testWidgets('Quran app launches successfully', (tester) async {
     await tester.pumpWidget(const QuranApp());
-    expect(find.text('القرآن الكريم'), findsOneWidget);
+
+    expect(find.byType(QuranApp), findsOneWidget);
   });
 }
